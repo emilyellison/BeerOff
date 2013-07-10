d62b3b08c047747f4efcdd5477028a3b7819bfab
Emily Ellison <emilyellison986@gmail.com>
2013-07-10 18:48:40 -0400

    abv and ibu

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,12 +3,20 @@ class CategoriesController < ApplicationController
   before_filter :fetch_categories
   
   def index
-    gon.abvs = StatBot.new(Beer.pluck(:abv), numerify: true, round: 0).freqs
+    if params[:measure] == "IBU"
+      gon.ibus = StatBot.new(Beer.pluck(:ibu), numerify: true, round: 0).freqs
+    else
+      gon.abvs = StatBot.new(Beer.pluck(:abv), numerify: true, round: 0).freqs
+    end
   end
   
   def show
     @category = Category.find(params[:id])
-    gon.abvs = StatBot.new(@category.beers.pluck(:abv), numerify: true, round: 0).freqs
+    if params[:measure] == "IBU"
+      gon.ibus = StatBot.new(@category.beers.pluck(:ibu), numerify: true, round: 0).freqs
+    else
+      gon.abvs = StatBot.new(@category.beers.pluck(:abv), numerify: true, round: 0).freqs
+    end
   end
   
   private
