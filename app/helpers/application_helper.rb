14252bbfe8dd5ba4c949e42ae11a490700ce8db1
Emily Ellison <emilyellison986@gmail.com>
2013-07-11 12:23:41 -0400

    clean up view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  # Active Measurement
+
+  def is_measuring?(measure, params)
+    "active" if params[:measure] == measure || is_default_measure?(measure, params)
+  end
+  
+  def is_default_measure?(measure, params)
+    params[:measure].nil? && measure == 'ABV'
+  end
+  
+  # Active Category
+  
+  def is_category?(category, params)
+    "active" if params[:controller] == 'categories' && (is_all_categories?(category, params) || params[:id] == category.try(:id).try(:to_s))
+  end
+  
+  def is_all_categories?(category, params)
+    params[:id].nil? && category == nil
+  end
+
 end
